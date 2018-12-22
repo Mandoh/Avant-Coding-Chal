b7022e3067d07ef95bb86fcb2dc615c556fa070a
mandoh <mandomartinez2017@gmail.com>
2018-12-22 14:22:25 -0600

    heroku deployment 7

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(routes)
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Avant",{ useNewUrlParser: true });
